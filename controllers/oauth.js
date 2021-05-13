b843e30df92d4e6c7146707f24659b3198b78407
mooon3356 <mooon3356@gmail.com>
2021-05-13 14:41:23 +0900

    Update oauth.js

--- controllers/oauth.js
+++ controllers/oauth.js
@@ -6,6 +6,7 @@ const axios = require("axios");
 
 module.exports = {
   github: (req, res) => {
+    console.log('요청은 들어옴')
     const code = req.body.authorizationCode;
 
     if (code) {
@@ -19,7 +20,15 @@ module.exports = {
         )
         .then((res) => res.data)
         .then((data) => {
-          res.status(200).send({ accessToken: data.access_token });
+          console.log(data.access_token)
+          if(data.access_token) {
+            res.status(200)
+            .cookie("accessToken", data.access_token , { httpOnly: true })
+            .send({ accessToken: data.access_token })
+          } else {
+            console.log('accessToken 없음')
+          }
+        
         });
     } else {
       res.status(404).send("no authorization code");
@@ -27,3 +36,8 @@ module.exports = {
   },
   kakao: (req, res) => {},
 };
+
+
+// res.status(200)
+//       .cookie("refreshToken", refreshToken, { httpOnly: true })
+//       .json({ data: { accessToken: accessToken }, message: "ok" });
