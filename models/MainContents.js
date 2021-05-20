4497224b9e8a26284934fc1793070eaf0287786e
Yongyong <yongs8425@gmail.com>
2021-05-20 14:12:48 +0900

    Fix kakao login error

--- models/MainContents.js
+++ models/MainContents.js
@@ -4,9 +4,11 @@ const MainContentSchema = new Schema(
   {
     title: { type: String, required: true },
     body: { type: String, required: true },
-    count: { type: Number, default: 0 },
+    count: { type: Number, required: true, default: 0 },
   },
   { timestamps: true }
 );
 
 module.exports = model("mainContents", MainContentSchema);
+
+//실제 컬렉션에서는 대문자를 넣으면 안됨. 값이 안뜸.
