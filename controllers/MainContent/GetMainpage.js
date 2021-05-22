eec9cf1390bd4d6f73988b416d8d8f4b96183161
Yongyong <yongs8425@gmail.com>
2021-05-22 13:33:11 +0900

    Add mainContent

--- controllers/MainContent/GetMainpage.js
+++ controllers/MainContent/GetMainpage.js
@@ -1,8 +1,8 @@
-const mainContents = require("../../models/MainContents");
+const MainContents = require("../../models/MainContents");
 
 module.exports = async (req, res) => {
   try {
-    const main = await mainContents.find();
+    const main = await MainContents.find();
     res.status(200).send(main);
   } catch (err) {
     res.status(500).send(err);
